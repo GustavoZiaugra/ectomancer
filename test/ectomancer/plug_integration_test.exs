c885f4bd61e70edfd7a15a9d4d7179cf495aec96
Gustavo Ziaugra <gustavo.ziaugra@live.com>
2026-07-20 12:40:26 -0400

    fix: suppress Logger.error in WebSocket connect test

--- test/ectomancer/plug_integration_test.exs
+++ test/ectomancer/plug_integration_test.exs
@@ -2,6 +2,7 @@ defmodule Ectomancer.PlugIntegrationTest do
   use ExUnit.Case
   import Plug.Conn
   import Plug.Test
+  import ExUnit.CaptureLog
 
   alias Ectomancer.Plug, as: EctomancerPlug
   alias Ectomancer.Plug.WebSocket, as: WSPlug
@@ -290,15 +291,20 @@ defmodule Ectomancer.PlugIntegrationTest do
     end
 
     test "connect with missing server option returns error" do
-      result =
-        WSPlug.connect(%{
+      ref = make_ref()
+
+      capture_log(fn ->
+        result = WSPlug.connect(%{
           endpoint: nil,
           transport: :websocket,
           params: %{},
           options: []
         })
 
-      assert result == {:error, :missing_server_option}
+        send(self(), {ref, result})
+      end)
+
+      assert_receive {^ref, {:error, :missing_server_option}}
     end
   end
 end
